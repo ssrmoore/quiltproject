dfe23c551d4bafda3ef5a80ea6f750c1fe4844e8
22skm4 <skm4@williams.edu>
2021-03-16 19:20:20 -0400

    rough copy of blue quilt!

--- quilt.py
+++ quilt.py
@@ -8,6 +8,13 @@ gold = '#F4D03F'
 black = '#0C0B0B'
 lightpurple = '#C089D3'
 
+aquamarine = '#51D3F7'
+darkblue = '#2D40A2'
+tan = '#DDBA7C'
+teal = '#0588AE'
+darkgreen = '#1D402F'
+navy = '#1F2B41'
+
 def initializeTurtle(size):
     """Sets up the window and initializes the turtle
     to be at the bottom left corner of the pattern
@@ -15,13 +22,14 @@ def initializeTurtle(size):
     padding = 25  # increase if patterns gets cut off
     # Create a turtle window
     setup(width = size + padding, height = size + padding)
+    bgcolor(navy)
     reset() # Clear any existing turtle drawings
             # and reset turtle position & heading.
-    pensize(1) # Choose a pen thickness
-    speed(6) # Set the speed; 0=fastest, 1=slowest, 6=normal
+    pensize(0) # Choose a pen thickness
+    speed(0) # Set the speed; 0=fastest, 1=slowest, 6=normal
     # By default turtle starts at (0,0): center of the screen
     # and by default faces east
-    
+
 
 def drawRectangle(size, color):
     """Draws a single square of side length size and given color
@@ -44,22 +52,27 @@ def testDrawQuilt(size):
     """Initializes turtle, and then calls drawRectangle multiple times in different places"""
     # initialize turtle
     initializeTurtle(size)
-    for _ in range(7):
+    for _ in range(20):
+        z = random.randint(1, 3)
         x = random.randint(1, 6)
-        if x % 4 == 0:
-            drawRectangle(size/x, purple)
-        elif x % 4 == 1:
-            drawRectangle(size/x, lightpurple)
-        elif x % 4 == 2:
-            drawRectangle(size/x, black)
-        elif x % 4 == 3:
-            drawRectangle(size/x, gold)
+        if x % 6 == 0:
+            drawRectangle(size/z, aquamarine)
+        elif x % 6 == 1:
+            drawRectangle(size/z, darkblue)
+        elif x % 6 == 2:
+            drawRectangle(size/z, tan)
+        elif x % 6 == 3:
+            drawRectangle(size/z, teal)
+        elif x % 6 == 4:
+            drawRectangle(size/z, darkgreen)
+        elif x % 6 == 5:
+            drawRectangle(size/z, navy)
         y = random.randint(1,size)
         fd(y/6)
         if y % 2 == 0:
             lt(90)
-            z = random.randint(1,size)
-            fd(z/6)
+            w = random.randint(1,size)
+            fd(w/6)
 
     # save the figure
     #getscreen().getcanvas().postscript(file="drawQuilt({},{}).eps".format(size))
